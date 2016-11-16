eb900194d2cf542b36b30fbb592f677616b32b06
Christian Lopez <cslg3000@gmail.com>
2016-11-15 21:21:23 -0300

    Se cambio el nombre de la instancia de crearO por agregarO en IAdministracionO.

--- src/Vista/IAdministracionO.java
+++ src/Vista/IAdministracionO.java
@@ -109,7 +109,7 @@ public class IAdministracionO extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        controlador.getInstancia().opcionSeleccionada("CrearO",this);
+        controlador.getInstancia().opcionSeleccionada("agregarO",this);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
